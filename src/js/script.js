f70b9cead07b2a352c6e92925e18d8defb1796ea
Marcin Hołubowicz <m.holubowicz87@gmail.com>
2021-03-20 20:27:59 +0100

    Add appendChild

--- src/js/script.js
+++ src/js/script.js
@@ -92,12 +92,16 @@
       const thisProduct = this;
 
       /* generateHTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
 
       /* create element using utills.createElementFromHtml */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
 
       /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
 
       /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
     }
   }
 
