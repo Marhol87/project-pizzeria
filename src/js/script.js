d38598fb224c17cd44de13c433e69fc76891825b
Marcin Hołubowicz <m.holubowicz87@gmail.com>
2021-03-21 18:28:58 +0100

    get elements

--- src/js/script.js
+++ src/js/script.js
@@ -129,11 +129,6 @@
     initAccordion() {
       const thisProduct = this;
 
-      /* find the clickable trigger (the element that should react to clicking) */
-      // const clickableTrigger = thisProduct.element.querySelector(
-      //   select.menuProduct.clickable
-      // );
-
       /* START: add event listener to clickable trigger on event click */
       thisProduct.accordionTrigger.addEventListener('click', function (event) {
         /* prevent default action for event */
