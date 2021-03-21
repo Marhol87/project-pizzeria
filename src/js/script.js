c895964a06a987640b060c3ff5dad17f3ca0c849
Marcin Hołubowicz <m.holubowicz87@gmail.com>
2021-03-21 18:48:42 +0100

    initOrderForm

--- src/js/script.js
+++ src/js/script.js
@@ -88,6 +88,8 @@
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
       console.log('new Product:', thisProduct);
     }
     renderInMenu() {
@@ -147,6 +149,31 @@
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle('active');
       });
+    }
+
+    initOrderForm() {
+      const thisProduct = this;
+      console.log(thisProduct);
+      thisProduct.form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for (let input of thisProduct.formInputs) {
+        input.addEventListener('change', function () {
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder() {
+      const thisProduct = this;
+      console.log(thisProduct);
     }
   }
 
