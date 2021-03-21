1bb36abe433095abcfebe69154ae8272c6774a2a
Marcin Hołubowicz <m.holubowicz87@gmail.com>
2021-03-21 18:21:46 +0100

    accordionTrigger

--- src/js/script.js
+++ src/js/script.js
@@ -85,8 +85,9 @@
       thisProduct.id = id;
       thisProduct.data = data;
 
-      thisProduct.initAccordion();
       thisProduct.renderInMenu();
+      thisProduct.getElements();
+      thisProduct.initAccordion();
       console.log('new Product:', thisProduct);
     }
     renderInMenu() {
@@ -105,16 +106,36 @@
       menuContainer.appendChild(thisProduct.element);
     }
 
+    getElements() {
+      const thisProduct = this;
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(
+        select.menuProduct.clickable
+      );
+      thisProduct.form = thisProduct.element.querySelector(
+        select.menuProduct.form
+      );
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(
+        select.all.formInputs
+      );
+      thisProduct.cartButton = thisProduct.element.querySelector(
+        select.menuProduct.cartButton
+      );
+      thisProduct.priceElem = thisProduct.element.querySelector(
+        select.menuProduct.priceElem
+      );
+    }
+
     initAccordion() {
       const thisProduct = this;
 
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector(
-        select.menuProduct.clickable
-      );
+      // const clickableTrigger = thisProduct.element.querySelector(
+      //   select.menuProduct.clickable
+      // );
 
       /* START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener('click', function (event) {
+      thisProduct.accordionTrigger.addEventListener('click', function (event) {
         /* prevent default action for event */
         event.preventDefault();
         /* find active product (product that has active class) */
@@ -123,11 +144,11 @@
         );
 
         /* if there is active product and it's not thisProduct.element, remove class active from it */
-
-        if (activeProduct !== null && activeProduct !== thisProduct.element) {
-          activeProduct.classList.remove('active');
+        for (let product of activeProduct) {
+          if (activeProduct !== null && activeProduct !== thisProduct.element) {
+            product.classList.remove('active');
+          }
         }
-
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle('active');
       });
