eb8c254a76ad1a3a8e92c1fa643e79beb569ebe8
Marcin Hołubowicz <m.holubowicz87@gmail.com>
2021-03-28 15:26:02 +0200

    thisWidget.setValue

--- src/js/script.js
+++ src/js/script.js
@@ -62,6 +62,7 @@
       console.log('constructor arguments:', element);
 
       thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
     }
     getElements(element) {
       const thisWidget = this;
@@ -81,7 +82,7 @@
       const thisWidget = this;
       const newValue = parseInt(value);
       /* TODO: Add validation */
-      if (thisWidget.value !== newValue) {
+      if (thisWidget.value !== newValue && !isNaN(newValue)) {
         thisWidget.value = newValue;
       }
       thisWidget.value = newValue;
