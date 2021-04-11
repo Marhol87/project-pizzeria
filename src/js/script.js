61b9f55e76cb5e7f7770cf9e71455ca39cb855d7
Marcin Hołubowicz <m.holubowicz87@gmail.com>
2021-04-11 19:06:21 +0200

    class CartProduct

--- src/js/script.js
+++ src/js/script.js
@@ -441,6 +441,18 @@
     }
   }
 
+  class CartProduct {
+    constructor(menuProduct, element) {
+      const thisCartProduct = this;
+      thisCartProduct.id = menuProduct.id;
+      thisCartProduct.name = menuProduct.name;
+      thisCartProduct.amount = menuProduct.amount;
+      thisCartProduct.priceSingle = menuProduct.priceSingle;
+      thisCartProduct.price = menuProduct.price;
+      thisCartProduct.params = menuProduct.params;
+    }
+  }
+
   const app = {
     initMenu: function () {
       const thisApp = this;
