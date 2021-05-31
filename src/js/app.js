344b5c63fea6c6efc5ff565c5a9a89c6e964acf7
Marcin Hołubowicz <m.holubowicz87@gmail.com>
2021-05-31 13:14:13 +0200

    Run thisApp.activatePage

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import { settings, select } from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 // import AmountWidget from './components/AmountWidget.js';
@@ -8,18 +8,40 @@ const app = {
   initPages: function () {
     const thisApp = this;
 
-    thisApp.pages = document.querySelector(selectcontainerOf.pages).children;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
 
     thisApp.activatePage(thisApp.pages[0].id);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get pageid from href atribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+      });
+    }
   },
 
   activatePage: function (pageId) {
     const thisApp = this;
 
     /* add class "active" to matching pages, remove from non-matching */
-    for(let page)
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
 
     /* add class "active" to matching links, remove from non-matching */
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
   },
 
   initMenu: function () {
